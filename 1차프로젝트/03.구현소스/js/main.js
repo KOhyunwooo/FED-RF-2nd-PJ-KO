228ff5baffb92c8b63660da3e9306f17a424500f
KO <j316ko@naver.com>
2024-04-05 16:29:56 +0900

    123123

--- 1차프로젝트/03.구현소스/js/main.js
+++ 1차프로젝트/03.구현소스/js/main.js
@@ -2,6 +2,11 @@
 
 // 모듈로 호출된 JS에서는 다른 외부JS를 import로 호출가능!
 // import하려는 파일에서 반드시 함수,변수 등을 export해야함!
+
+//부드러운 스크롤 작동함수 호출!
+import{startSS, setScrollPos} from "./smoothScroll23.js";
+startSS();
+
 import following from "./following_circle.js";
 // slideFn 슬라이드 기능함수 호출!
 following();
