8f3eb62decca2ef09dc1f1935b2ace6ed3d269d4
j316 <j316ko@naver.com>
2024-06-16 00:03:29 +0900

    124

---
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/data/banner_data.js
@@ -0,0 +1,92 @@
+//gnb클릭시 들어가는 배너 데이터
+export const banner_data = {
+  womanData: [
+    {
+      sale_imgsrc: "",
+      sale_txt: "",
+    },
+    {
+      newarr_imgsrc: "",
+      newarr_txt: "",
+    },
+    {
+      first_imgsrc: "",
+      first_txt: "",
+    },
+    {
+      second_imgsrc: "",
+      second_txt: "",
+    },
+    {
+      third_imgsrc: "",
+      third_imgsrc_txt: "",
+    },
+  ],
+  manData: [
+    {
+      sale_imgsrc: "",
+      sale_txt: "",
+    },
+    {
+      newarr_imgsrc: "",
+      newarr_txt: "",
+    },
+    {
+      first_imgsrc: "",
+      first_txt: "",
+    },
+    {
+      second_imgsrc: "",
+      second_txt: "",
+    },
+    {
+      third_imgsrc: "",
+      third_imgsrc_txt: "",
+    },
+  ],
+  kidsData: [
+    {
+      sale_imgsrc: "",
+      sale_txt: "",
+    },
+    {
+      newarr_imgsrc: "",
+      newarr_txt: "",
+    },
+    {
+      first_imgsrc: "",
+      first_txt: "",
+    },
+    {
+      second_imgsrc: "",
+      second_txt: "",
+    },
+    {
+      third_imgsrc: "",
+      third_imgsrc_txt: "",
+    },
+  ],
+  homeData: [
+    {
+      sale_imgsrc: "",
+      sale_txt: "",
+    },
+    {
+      newarr_imgsrc: "",
+      newarr_txt: "",
+    },
+    {
+      first_imgsrc: "",
+      first_txt: "",
+    },
+    {
+      second_imgsrc: "",
+      second_txt: "",
+    },
+    {
+      third_imgsrc: "",
+      third_imgsrc_txt: "",
+    },
+  ],
+ 
+};
--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/plugin/SwiperMain.jsx
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/plugin/SwiperMain.jsx
@@ -14,123 +14,122 @@ import { Pagination, Navigation, Autoplay, Mousewheel } from "swiper/modules";
 import { main_img } from "../data/main_img";
 
 export default function SwiperMain() {
-    return (
-        <>
-            {/* 가로방향 스와이프 설정///////////////////// */}
-            <Swiper
-                className="mySwiper swiper-h"
-                spaceBetween={0}
-                pagination={{
-                    clickable: true,
-                }}
-                navigation={true}
-                //맨페이지부터 시작하게 하기
-                initialSlide={1}
-                modules={[Autoplay, Pagination, Navigation, Mousewheel]}
-            >
-                <SwiperSlide>
-                    {/* 세로방향 스와이프 설정///////////////////// */}
-                    <Swiper
-                        className="mySwiper2 swiper-v"
-                        direction={"vertical"}
-                        spaceBetween={0}
-                        mousewheel={true}
-                        autoplay={{
-                            delay: 2000,
-                            disableOnInteraction: false,
-                            //마지막 페이지에 도달하면 멈추기
-                            stopOnLastSlide: true,
-                        }}
-                        pagination={{
-                            clickable: true,
-                        }}
-                        // navigation={true}
-                        modules={[Autoplay, Pagination, Navigation, Mousewheel]}
-                    >
-                                                          
-                        {main_img.main_woman.map((v, i) => (
-                            <SwiperSlide key={i}>
-                                <img src={v.isrc} alt={v.tit} />
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Swiper
-                        className="mySwiper2 swiper-v"
-                        direction={"vertical"}
-                        spaceBetween={0}
-                        mousewheel={true}
-                        autoplay={{
-                            delay: 2000,
-                            disableOnInteraction: false,
-                            //마지막 페이지에 도달하면 멈추기
-                            stopOnLastSlide: true,
-                        }}
-                        pagination={{
-                            clickable: true,
-                        }}
-                        // navigation={true}
-                        modules={[Autoplay, Pagination, Navigation, Mousewheel]}
-                    >
-                        {main_img.main_man.map((v, i) => (
-                            <SwiperSlide key={i}>
-                                <img src={v.isrc} alt={v.tit} />
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Swiper
-                        className="mySwiper2 swiper-v"
-                        direction={"vertical"}
-                        spaceBetween={0}
-                        mousewheel={true}
-                        autoplay={{
-                            delay: 2000,
-                            disableOnInteraction: false,
-                            //마지막 페이지에 도달하면 멈추기
-                            stopOnLastSlide: true,
-                        }}
-                        pagination={{
-                            clickable: true,
-                        }}
-                        // navigation={true}
-                        modules={[Autoplay, Pagination, Navigation, Mousewheel]}
-                    >
-                        {main_img.main_kids.map((v, i) => (
-                            <SwiperSlide key={i}>
-                                <img src={v.isrc} alt={v.tit} />
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Swiper
-                        className="mySwiper2 swiper-v"
-                        direction={"vertical"}
-                        spaceBetween={0}
-                        mousewheel={true}
-                        autoplay={{
-                            delay: 2000,
-                            disableOnInteraction: false,
-                            //마지막 페이지에 도달하면 멈추기
-                            stopOnLastSlide: true,
-                        }}
-                        pagination={{
-                            clickable: true,
-                        }}
-                        // navigation={true}
-                        modules={[Autoplay, Pagination, Navigation, Mousewheel]}
-                    >
-                        {main_img.main_home.map((v, i) => (
-                            <SwiperSlide key={i}>
-                                <img src={v.isrc} alt={v.tit} />
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
-                </SwiperSlide>
-            </Swiper>
-        </>
-    );
+  return (
+    <>
+      {/* 가로방향 스와이프 설정///////////////////// */}
+      <Swiper
+        className="mySwiper swiper-h"
+        spaceBetween={0}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        //맨페이지부터 시작하게 하기
+        initialSlide={1}
+        modules={[Autoplay, Pagination, Navigation, Mousewheel]}
+      >
+        <SwiperSlide>
+          {/* 세로방향 스와이프 설정///////////////////// */}
+          <Swiper
+            className="mySwiper2 swiper-v"
+            direction={"vertical"}
+            spaceBetween={0}
+            mousewheel={true}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+              //마지막 페이지에 도달하면 멈추기
+              stopOnLastSlide: true,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            // navigation={true}
+            modules={[Autoplay, Pagination, Navigation, Mousewheel]}
+          >
+            {main_img.main_woman.map((v, i) => (
+              <SwiperSlide key={i}>
+                <img src={v.isrc} alt={v.tit} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Swiper
+            className="mySwiper2 swiper-v"
+            direction={"vertical"}
+            spaceBetween={0}
+            mousewheel={true}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+              //마지막 페이지에 도달하면 멈추기
+              stopOnLastSlide: true,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            // navigation={true}
+            modules={[Autoplay, Pagination, Navigation, Mousewheel]}
+          >
+            {main_img.main_man.map((v, i) => (
+              <SwiperSlide key={i}>
+                <img src={v.isrc} alt={v.tit} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Swiper
+            className="mySwiper2 swiper-v"
+            direction={"vertical"}
+            spaceBetween={0}
+            mousewheel={true}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+              //마지막 페이지에 도달하면 멈추기
+              stopOnLastSlide: true,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            // navigation={true}
+            modules={[Autoplay, Pagination, Navigation, Mousewheel]}
+          >
+            {main_img.main_kids.map((v, i) => (
+              <SwiperSlide key={i}>
+                <img src={v.isrc} alt={v.tit} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Swiper
+            className="mySwiper2 swiper-v"
+            direction={"vertical"}
+            spaceBetween={0}
+            mousewheel={true}
+            autoplay={{
+              delay: 2000,
+              disableOnInteraction: false,
+              //마지막 페이지에 도달하면 멈추기
+              stopOnLastSlide: true,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            // navigation={true}
+            modules={[Autoplay, Pagination, Navigation, Mousewheel]}
+          >
+            {main_img.main_home.map((v, i) => (
+              <SwiperSlide key={i}>
+                <img src={v.isrc} alt={v.tit} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
