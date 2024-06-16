5b1d7bd537e879a66ff1476c6f4ce0685c5375fc
j316 <j316ko@naver.com>
2024-06-16 22:10:19 +0900

    124

---
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/data/banner_data copy.js
@@ -0,0 +1,112 @@
+//gnb클릭시 들어가는 배너 데이터
+export const banner_data = {
+  womanData: [
+    {
+      sale_imgsrc: "images/banner_img/woman_sale.jpg",
+      sale_txt: "세일 상품이 추가되었습니다",
+      link:"/man",
+    },
+    {
+      newarr_imgsrc: "images/banner_img/woman_new.jpg",
+      newarr_txt: "New arrivals",
+      link:"/home",
+    },
+    {
+      first_imgsrc: "images/banner_img/woman_first.jpg",
+      first_txt: "원피스",
+      link:"/#",
+    },
+    {
+      second_imgsrc: "images/banner_img/woman_second.jpg",
+      second_txt: "탑",
+      link:"/#",
+    },
+    {
+      third_imgsrc: "images/banner_img/woman_third.jpg",
+      third_txt: "셔츠",
+      link:"/#",
+    },
+  ],
+  manData: [
+    {
+      sale_imgsrc: "images/banner_img/man_sale.jpg",
+      sale_txt: "세일 상품이 추가되었습니다",
+      link:"/#",
+    },
+    {
+      newarr_imgsrc: "images/banner_img/man_new.jpg",
+      newarr_txt: "New arrivals",
+      link:"/#",
+    },
+    {
+      first_imgsrc: "images/banner_img/man_first.jpg",
+      first_txt: "셔츠",
+      link:"/#",
+    },
+    {
+      second_imgsrc: "images/banner_img/man_second.jpg",
+      second_txt: "블레이저",
+      link:"/#",
+    },
+    {
+      third_imgsrc: "images/banner_img/man_third.jpg",
+      third_txt: "니트",
+      link:"/#",
+    },
+  ],
+  kidsData: [
+    {
+      sale_imgsrc: "images/banner_img/kids_sale.jpg",
+      sale_txt: "세일 상품이 추가되었습니다",
+      link:"/#",
+    },
+    {
+      newarr_imgsrc: "images/banner_img/kids_new.jpg",
+      newarr_txt: "New arrivals",
+      link:"/#",
+    },
+    {
+      first_imgsrc: "images/banner_img/kids_first.jpg",
+      first_txt: "",
+      link:"/#",
+    },
+    {
+      second_imgsrc: "images/banner_img/kids_second.jpg",
+      second_txt: "",
+      link:"/#",
+    },
+    {
+      third_imgsrc: "images/banner_img/kids_third.jpg",
+      third_txt: "",
+      link:"/#",
+    },
+  ],
+  homeData: [
+    {
+      sale_imgsrc: "images/banner_img/home_sale.jpg",
+      sale_txt: "(160 GXM²) 워시드 린넨 침구",
+      link:"/#",
+    },
+    {
+      newarr_imgsrc: "images/banner_img/home_new.jpg",
+      newarr_txt: "New arrivals: STOOL 03",
+      link:"/#",
+    },
+    {
+      first_imgsrc: "images/banner_img/home_first.jpg",
+      first_txt: "페이크 시어링 반려동물 침대",
+      link:"/#",
+    },
+    {
+      second_imgsrc: "images/banner_img/home_second.jpg",
+      second_txt: "주방 휴지통",
+      link:"/#",
+    },
+    {
+      third_imgsrc: "images/banner_img/home_third.jpg",
+      third_txt: "언이븐 아카시아 커피 테이블",
+      link:"/#",
+    },
+  ],
+ 
+};
--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/modules/Banner.jsx
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/modules/Banner.jsx
@@ -8,73 +8,25 @@ const Banner = ({ category }) => {
 
   return (
     <div className="banner">
-      <div className="first-div">
-        {selData.map(
-          (v, i) =>
-            (v.sale_imgsrc || v.newarr_imgsrc) && (
-              <div key={i}>
-                <Link to={v.link}>
-                  {v.sale_imgsrc && (
-                    <img src={v.sale_imgsrc} alt={v.sale_txt} />
-                  )}
-                  <p>{v.sale_txt}</p>
-                </Link>
-                <Link to={v.link}>
-                  {v.newarr_imgsrc && (
-                    <img src={v.newarr_imgsrc} alt={v.newarr_txt} />
-                  )}
-                  <p>{v.newarr_txt}</p>
-                </Link>
-              </div>
-            )
-          //   <div key={i}>
-          //     {v.sale_imgsrc && (
-          //       <img src={v.sale_imgsrc} alt={v.sale_txt} />
-          //     )}
-          //     {v.newarr_imgsrc && (
-          //       <img src={v.newarr_imgsrc} alt={v.newarr_txt} />
-          //     )}
-
-          //   </div>
-        )}
+      <div className="top-images">
+        {selData.slice(0, 2).map((v, i) => (
+          <div key={i}>
+            <Link to={v.link}>
+              <img src={v.imgsrc} alt={v.txt} />
+              <p>{v.txt}</p>
+            </Link>
+          </div>
+        ))}
       </div>
-      <div className="second-div">
-        {selData.map(
-          (v, i) =>
-            (v.first_imgsrc || v.second_imgsrc || v.third_imgsrc) && (
-              <div key={i}>
-                <Link to={v.link}>
-                  {v.first_imgsrc && (
-                    <img src={v.first_imgsrc} alt={v.first_txt} />
-                  )}
-                  <p>{v.first_txt}</p>
-                </Link>
-                <Link to={v.link}>
-                  {v.second_imgsrc && (
-                    <img src={v.second_imgsrc} alt={v.second_txt} />
-                  )}
-                  <p>{v.second_txt}</p>
-                </Link>
-                <Link to={v.link}>
-                  {v.third_imgsrc && (
-                    <img src={v.third_imgsrc} alt={v.third_txt} />
-                  )}
-                  <p>{v.third_txt}</p>
-                </Link>
-              </div>
-            )
-          //   <div key={i}>
-          //     {v.first_imgsrc && (
-          //       <img src={v.first_imgsrc} alt={v.first_txt} />
-          //     )}
-          //     {v.second_imgsrc && (
-          //       <img src={v.second_imgsrc} alt={v.second_txt} />
-          //     )}
-          //     {v.third_imgsrc && (
-          //       <img src={v.third_imgsrc} alt={v.third_txt} />
-          //     )}
-          //   </div>
-        )}
+      <div className="bottom-images">
+        {selData.slice(2).map((v, i) => (
+          <div key={i}>
+            <Link to={v.link}>
+              <img src={v.imgsrc} alt={v.txt} />
+              <p>{v.txt}</p>
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
