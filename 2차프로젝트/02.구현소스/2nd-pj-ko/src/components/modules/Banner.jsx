07460d84401128129ddb0b7d1a7d954ba9baa0d1
KO <j316ko@naver.com>
2024-06-17 11:56:47 +0900

    124124

--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/modules/Banner.jsx
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/modules/Banner.jsx
@@ -8,24 +8,35 @@ const Banner = ({ category }) => {
 
   return (
     <div className="banner">
+      {/* 위에 사진 2개 부분*/}
       <div className="top-images">
-        {selData.slice(0, 2).map((v, i) => (
+      {/* {selData.slice(0, 2).map((v, i) => (
           <div key={i}>
             <Link to={v.link}>
               <img src={v.imgsrc} alt={v.txt} />
               <p>{v.txt}</p>
             </Link>
           </div>
+        ))} 
+        슬라이스로 셀데이터0~2까지 잘라도 가능,2는 포함x*/}
+        {selData.map((v, i) => (
+          i<2&&(<div key={i}>
+            <Link to={v.link}>
+              <img src={v.imgsrc} alt={v.txt} />
+              <p>{v.txt}</p>
+            </Link>
+          </div>)
         ))}
       </div>
+      {/* 아래 사진 3개 부분 */}
       <div className="bottom-images">
-        {selData.slice(2).map((v, i) => (
-          <div key={i}>
+        {selData.map((v, i) => (
+         i>=2&&( <div key={i>=3}>
             <Link to={v.link}>
               <img src={v.imgsrc} alt={v.txt} />
               <p>{v.txt}</p>
             </Link>
-          </div>
+          </div>)
         ))}
       </div>
     </div>
