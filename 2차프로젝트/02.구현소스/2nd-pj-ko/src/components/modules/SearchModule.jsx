700ff2e24c1a5537d51194fdc7fd29edd3e2fe6f
KO <j316ko@naver.com>
2024-06-18 14:32:29 +0900

    124

---
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/modules/SearchModule.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchModule(props) {
+    return (
+        <div>
+            <h1>난 서치모듈:검색모듈</h1>
+        </div>
+    );
+}
+
+export default SearchModule;
---
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/pages/SearchPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchModule from '../modules/SearchModule';
+
+function SearchPage(props) {
+    return (
+        <div>
+            <h1>난 서치 페이지</h1>
+            <SearchModule/>
+        </div>
+    );
+}
+
+export default SearchPage;
