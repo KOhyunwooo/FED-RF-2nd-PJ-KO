b55e57efa3667a5fc9964a4f868a5f888f7a7436
j316 <j316ko@naver.com>
2024-06-23 18:57:30 +0900

    124

--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/layout/TopAreaMedia.jsx
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/layout/TopAreaMedia.jsx
@@ -11,35 +11,34 @@ import { gnbData } from "../data/gnb_data";
 import { Link } from "react-router-dom";
 
 function TopAreaMedia() {
-
-  const [selectedIndex, setSelectedIndex] = useState(0);// 선택된 상위 메뉴 인덱스 상태 관리
+  const [selectedIndex, setSelectedIndex] = useState(0); // 선택된 상위 메뉴 인덱스 상태 관리,상태를 통해 현재 선택된 메뉴의 인덱스를 저장.
 
   const 클릭된순번 = (index) => {
-    setSelectedIndex(index);// 메뉴 클릭 시 선택된 인덱스 업데이트
+    setSelectedIndex(index); // 메뉴 클릭 시 선택된 인덱스 업데이트,
   };
   // 메뉴 여닫이 함수/////////////////////////////////////////////////////////////////////////////////
   const showHideMenu = (e) => {
     console.log("나는 showHideMenu", e.currentTarget); //<- 나 자신을 콘솔찍기
     // 1.전체메뉴 대상: .mbox
-    $(".mbox").fadeToggle(200);
+    $(".mbox").toggleClass("on");
     //fadeIn() 서서히 나타남
     //fadeOut() 서서히 사라짐 -> display:none
     //fadeToggle() 서서히 나타남/ 사라짐 전환
 
     // 2.햄버거 버튼에 클래스 "on"넣기/ 뺴기
-    $(e.currentTarget).toggleClass("on");
+    // $(e.currentTarget).toggleClass("on");
     // addClass() 클래스 넣기
     // removeClass() 클래스 뺴기
     // toggleClass() 클래스 넣기/ 뺴기
 
-    console.log($(e.currentTarget).is(".on")); //<-e.currentTarget에 .on 이 있냐?
+    // console.log($(e.currentTarget).is(".on")); //<-e.currentTarget에 .on 이 있냐?
   };
 
   console.log(gnbData);
   return (
     <>
       <nav className="hamgnb">
-        {/* 박스1: 자라로고, 로그인, 바스켓백, 햄버거버튼 */}
+        {/* 박스1: 자라로고, 로그인, 바스켓백, 햄버거버튼///////////////////////////////////////////// */}
         <div className="box1">
           <Link to="/">
             <img
@@ -60,75 +59,63 @@ function TopAreaMedia() {
               </span>
             </Link>
           </div>
+          {/* 햄버거 버튼 */}
           <div className="ham" onClick={showHideMenu}>
             <span></span> <span></span> <span></span>
           </div>
         </div>
-        {/* 박스2: gnb상위메뉴 */}
+
+        {/* 박스2: gnb메뉴////////////////////////////////////////////////////////////////////////////// */}
         <div className="box2">
           <div className="mbox">
-          {/* ------------------------------------------------------------------- */}
-          <div className="menu-container">
-      <ul className="gnb-list">
-        {gnbData.map((v, i) => (
-          <li key={i} className="gnb-item">
-            <button
-              //selectedIndex 가 i 와 같으면 클래스네임gnblink에 on클래스 추가
-              className={`gnb-link ${selectedIndex === i ? "on" : ""}`}
-              onClick={() => 클릭된순번(i)}//button 온클릭시 클릭된순번(i)실행
-            >
-              {v.txt}
-            </button>
-          </li>
-        ))}
-      </ul>
-      <div className="sub-menu-container">
-        <div
-          className="sub-menu-wrapper"//순번 가로로 400vw 만듬
-          style={{ transform: `translateX(-${selectedIndex * 100}vw)` }}//선택된순번*100 해서 vw값 변환
-        >
-          {gnbData.map((v, i) => (
-            <div key={i} className="sub-menu">
-              {v.sub && (
-                <aside className="smbx">
-                  <ol>
-                    {v.sub.map((v, i) => (
-                      <li key={i}>
-                        <Link
-                          to={v.link}
-                          state={{ data: v.data }}
-                          style={{ color: v.txt === "특가 상품" ? "red" : "" }}
-                        >
-                          {v.txt}
-                        </Link>
-                      </li>
-                    ))}
-                  </ol>
-                </aside>
-              )}
+            {/* ------------------------------------------------------------------- */}
+            <div className="menu-container">
+              <ul className="gnb-list">
+                {gnbData.map((v, i) => (
+                  <li key={i} className="gnb-item">
+                    <button
+                      //selectedIndex 가 i 와 같으면 클래스네임gnblink에 on클래스 추가
+                      className={`gnb-link ${selectedIndex === i ? "on" : ""}`}
+                      onClick={() => 클릭된순번(i)} //button 온클릭시 클릭된순번(i)실행
+                    >
+                      {v.txt}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+              <div className="sub-menu-container">
+                <div
+                  className="sub-menu-wrapper" //순번 가로로 400vw 만듬
+                  style={{ transform: `translateX(-${selectedIndex * 100}vw)` }} //선택된순번*100 해서 vw값 변환
+                >
+                  {gnbData.map((v, i) => (
+                    <div key={i} className="sub-menu">
+                      {v.sub && (
+                        <aside className="smbx">
+                          <ol>
+                            {v.sub.map((v, i) => (
+                              <li key={i}>
+                                <Link
+                                  to={v.link}
+                                  state={{ data: v.data }}
+                                  style={{
+                                    color: v.txt === "특가 상품" ? "red" : "",
+                                  }}
+                                >
+                                  {v.txt}
+                                </Link>
+                              </li>
+                            ))}
+                          </ol>
+                        </aside>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
-          ))}
-        </div>
-      </div>
-    </div>
           </div>
           {/* ------------------------------------------------------------- */}
-        </div>
-        {/* 박스3: gnb하위메뉴 */}
-        <div>
-
-
-        </div>
-
-        {/* 박스4: 검색 창 */}
-            <input
-              type="text"
-              name="schinGnb" /* name은 백엔드 개발자를 위한 약속, 보통id랑 같은이름으로 함 */
-              id="schinGnb"
-              placeholder="검색"
-              
-            />
-        <div className="box4">
         </div>
       </nav>
     </>
