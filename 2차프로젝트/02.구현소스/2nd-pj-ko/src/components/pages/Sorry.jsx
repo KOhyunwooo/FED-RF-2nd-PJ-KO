4955333e267731342efe02e5ad2d7a9b733f0ee2
j316 <j316ko@naver.com>
2024-06-25 09:55:57 +0900

    124

--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/pages/Sorry.jsx
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/pages/Sorry.jsx
@@ -4,8 +4,10 @@ function Sorry() {
     return (
        <>
         <br /><br /><br /><br /><br /><br /><br /><br />
-        <h1 style={{fontSize:"50px", textAlign:"center"}}>여기는 아직 준비되지 않았습니다만?!...</h1>
-        <img src="./images/sorry.jpg" alt="자라" />
+        <div style={{width:"80vw",margin:"0 auto"}}>
+            <h1 style={{fontSize:"30px", textAlign:"center"}}>여기는 아직 준비되지 않았습니다만?!...</h1>
+            <img style={{width:"100%"}} src="./images/sorry.jpg" alt="자라" />
+        </div>
        </>
     );
 }
