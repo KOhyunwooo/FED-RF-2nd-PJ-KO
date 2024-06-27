973472d0818e5dcdae811a6993485eb0e159de0a
j316 <j316ko@naver.com>
2024-06-28 04:49:58 +0900

    124

--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/data/banner_data.js
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/data/banner_data.js
@@ -2,31 +2,31 @@
 export const banner_data = {
   womanData: [
     {
-      imgsrc: "images/banner_img/woman_sale.jpg",
+      imgsrc: "/images/banner_img/woman_sale.jpg",
       txt: "세일 상품이 추가되었습니다",
       link: "/woman/product",
       data: "wSale",
     },
     {
-      imgsrc: "images/banner_img/woman_new.jpg",
+      imgsrc: "/images/banner_img/woman_new.jpg",
       txt: "New arrivals",
       link: "/woman/product",
       data: "wNew",
     },
     {
-      imgsrc: "images/banner_img/woman_first.jpg",
+      imgsrc: "/images/banner_img/woman_first.jpg",
       txt: "원피스",
       link: "/sorry",
       data: "wSale",
     },
     {
-      imgsrc: "images/banner_img/woman_second.jpg",
+      imgsrc: "/images/banner_img/woman_second.jpg",
       txt: "탑",
       link: "/sorry",
       data: "wTop",
     },
     {
-      imgsrc: "images/banner_img/woman_third.jpg",
+      imgsrc: "/images/banner_img/woman_third.jpg",
       txt: "셔츠",
       link: "/sorry",
       data: "wShirts",
@@ -34,31 +34,31 @@ export const banner_data = {
   ],
   manData: [
     {
-      imgsrc: "images/banner_img/man_sale.jpg",
+      imgsrc: "/images/banner_img/man_sale.jpg",
       txt: "세일 상품이 추가되었습니다",
       link: "/man/product",
       data: "mSale",
     },
     {
-      imgsrc: "images/banner_img/man_new.jpg",
+      imgsrc: "/images/banner_img/man_new.jpg",
       txt: "New arrivals",
       link: "/man/product",
       data: "mNew",
     },
     {
-      imgsrc: "images/banner_img/man_first.jpg",
+      imgsrc: "/images/banner_img/man_first.jpg",
       txt: "셔츠",
       link: "/#",
       data: "mShirts",
     },
     {
-      imgsrc: "images/banner_img/man_second.jpg",
+      imgsrc: "/images/banner_img/man_second.jpg",
       txt: "블레이저",
       link: "/#",
       data: "mBlazer",
     },
     {
-      imgsrc: "images/banner_img/man_third.jpg",
+      imgsrc: "/images/banner_img/man_third.jpg",
       txt: "니트",
       link: "/#",
       data: "mKnit",
@@ -66,31 +66,31 @@ export const banner_data = {
   ],
   kidsData: [
     {
-      imgsrc: "images/banner_img/girl.jpg",
+      imgsrc: "/images/banner_img/girl.jpg",
       txt: "여아",
       link: "/#",
       data: "kSale",
     },
     {
-      imgsrc: "images/banner_img/boy.jpg",
+      imgsrc: "/images/banner_img/boy.jpg",
       txt: "남아",
       link: "/#",
       data: "kNew",
     },
     {
-      imgsrc: "images/banner_img/hat.jpg",
+      imgsrc: "/images/banner_img/hat.jpg",
       txt: "모자",
       link: "/#",
       data: "",
     },
     {
-      imgsrc: "images/banner_img/nba.jpg",
+      imgsrc: "/images/banner_img/nba.jpg",
       txt: "NBA colletion",
       link: "/#",
       data: "",
     },
     {
-      imgsrc: "images/banner_img/jamot.jpg",
+      imgsrc: "/images/banner_img/jamot.jpg",
       txt: "잠옷",
       link: "/#",
       data: "",
@@ -98,31 +98,31 @@ export const banner_data = {
   ],
   homeData: [
     {
-      imgsrc: "images/banner_img/home_sale.jpg",
+      imgsrc: "/images/banner_img/home_sale.jpg",
       txt: "(160 GXM²) 워시드 린넨 침구",
       link: "/#",
       data: "hSale",
     },
     {
-      imgsrc: "images/banner_img/home_new.jpg",
+      imgsrc: "/images/banner_img/home_new.jpg",
       txt: "New arrivals: STOOL 03",
       link: "/#",
       data: "hNew",
     },
     {
-      imgsrc: "images/banner_img/home_first.jpg",
+      imgsrc: "/images/banner_img/home_first.jpg",
       txt: "페이크 시어링 반려동물 침대",
       link: "/#",
       data: "",
     },
     {
-      imgsrc: "images/banner_img/home_second.jpg",
+      imgsrc: "/images/banner_img/home_second.jpg",
       txt: "주방 휴지통",
       link: "/#",
       data: "",
     },
     {
-      imgsrc: "images/banner_img/home_third.jpg",
+      imgsrc: "/images/banner_img/home_third.jpg",
       txt: "언이븐 아카시아 커피 테이블",
       link: "/#",
       data: "",
