a182f205ee48ca56dcd0d1e5647470509caf8ac7
KO <j316ko@naver.com>
2024-06-27 17:14:11 +0900

    124

--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/modules/ProductList.jsx
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/modules/ProductList.jsx
@@ -7,6 +7,8 @@ import { mBestSeller,mNew,mSale,mOrigins } from "../data/products_man";
 import "../../css/ProductList.scss";
 function ProductList({ dbName }) {
   const selData = { wNew: wNew, wSale: wSale, mBestSeller: mBestSeller,mNew:mNew,mSale:mSale,mOrigins:mOrigins };
+  //모아진 셀데이터를 역순으로만들기(이유: 신상품순으로 돌게 하기 위해서)
+  const reversedData = selData[dbName].slice().reverse();
   //내가 사용하는 배열데이터랑 db데이터랑 일치하는 객체를 만들어준다.
   //selData
   console.log(selData);
@@ -15,7 +17,7 @@ function ProductList({ dbName }) {
 
   return (
     <div className="product-list">
-      {selData[dbName].map((v, i) => (
+      {reversedData.map((v, i) => (
         <div key={i} className="product-item">
           <img src={v.isrc} alt={v.name} className="product-image" />
           <div className="txt-box">
