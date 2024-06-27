827cc00d3aa4da7521bbad463fbbf2917beb2b56
j316 <j316ko@naver.com>
2024-06-28 05:08:53 +0900

    124

--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/pages/Sorry.jsx
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/pages/Sorry.jsx
@@ -3,16 +3,22 @@ import React, { useEffect } from 'react';
 function Sorry() {
     useEffect(() => {
         window.scrollTo(0, 0);
-      },[]);
-  
+    }, []);
+
+    const randomzaraimg = `/images/sorry${Math.floor(Math.random() * 3) + 1}.jpg`;
+
     return (
-       <>
-        <br /><br /><br /><br /><br /><br /><br /><br />
-        <div style={{width:"80vw",margin:"0 auto"}}>
-            <h1 style={{fontSize:"30px", textAlign:"center"}}>여기는 아직 준비되지 않았습니다만?!...</h1>
-            <img style={{width:"100%",maxWidth:"500px",margin:'0 auto',display:"block"}} src={process.env.PUBLIC_URL+`/images/sorry${Math.floor(Math.random() * 3) + 1}.jpg`} alt="자라" />
-        </div>
-       </>
+        <>
+            <br /><br /><br /><br /><br /><br /><br /><br />
+            <div style={{ width: "80vw", margin: "0 auto" }}>
+                <h1 style={{ fontSize: "30px", textAlign: "center" }}>여기는 아직 준비되지 않았습니다만?!...</h1>
+                <img
+                    style={{ width: "100%", maxWidth: "500px", margin: '0 auto', display: "block" }}
+                    src={process.env.PUBLIC_URL+randomzaraimg}
+                    alt="응애~"
+                />
+            </div>
+        </>
     );
 }
 
