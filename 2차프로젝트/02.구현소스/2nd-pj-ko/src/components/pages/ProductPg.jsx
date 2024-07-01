c9f48e5d55630ab0915bbb5b1079f440e1533845
j316 <j316ko@naver.com>
2024-07-01 22:54:05 +0900

    124

--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/pages/ProductPg.jsx
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/pages/ProductPg.jsx
@@ -13,15 +13,15 @@ function ProductPg({ cat }) {
   let data = loc.state.data;
 
   const chgBgFn = () => {
-    if(data!="man") return;
+    if(cat!=="man")return;//카테고리가 man이 아니면 함수 종료
     let olist = document.querySelector(".origins-list");
     let bgTg = document.querySelector(".cont");
+
     
-  
-    
-    // let winH = window.innerHeight*1; 
-    const winH = document.documentElement.clientHeight*0.9;
-     //window.innerHeight*1보다 더 일관된 높이 계산:브라우저의 주소 창이나 스크롤바와 같은 UI 요소를 더 정확히 고려함.///////////////////
+
+    // let winH = window.innerHeight*1;
+    const winH = document.documentElement.clientHeight * 0.9;
+    //window.innerHeight*1보다 더 일관된 높이 계산:브라우저의 주소 창이나 스크롤바와 같은 UI 요소를 더 정확히 고려함.///////////////////
 
     let pos = olist.getBoundingClientRect().top;
     console.log(pos);
@@ -35,33 +35,30 @@ function ProductPg({ cat }) {
   };
 
   useEffect(() => {
-    
-
-
     console.log("^^^^^useEffect실행");
     // 남자페이지에서 있을때만 이벤트 설정
     if (document.querySelector(".origins-list")) {
       window.addEventListener("scroll", chgBgFn);
+     
     }
 
     // 없는 페이지는 중간에 남자 페이지에서 이동할 경우
     // 이벤트를 삭제해야함!
-    else{
+    else {
+      window.removeEventListener("scroll", chgBgFn);
 
-        window.removeEventListener("scroll", chgBgFn);
-        window.addEventListener("touchmove", chgBgFn);//모바일 작동용 터치무브////////////////////////
-        document.querySelector(".cont").classList.remove("change");
-        // document.style.backgroundColor = "transparent";
+      document.querySelector(".cont").classList.remove("change");
+      // document.style.backgroundColor = "transparent";
     }
 
     return () => {
-        console.log("소멸실행!");
-        window.removeEventListener("scroll", chgBgFn);
-        window.removeEventListener("touchmove", chgBgFn);//모바일 작동용 터치무브//////////////////////////////
-        document.querySelector(".cont").classList.remove("change");
-        // document.style.backgroundColor = "transparent";
+      console.log("소멸실행!");
+      window.removeEventListener("scroll", chgBgFn);
+
+      document.querySelector(".cont").classList.remove("change");
+      // document.style.backgroundColor = "transparent";
     };
-  },[]);
+  }, [cat]);//cat 이 변경될때마다 소멸 실행
 
   const chgBg = () => {}; /////////// chgBg //////////////
 
