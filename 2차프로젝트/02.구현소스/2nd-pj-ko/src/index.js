c64ff621ae4377f54bbd4e7820bbe45e00cf4f56
Tom B. Parker <tombap8@hotmail.com>
2024-07-01 18:18:39 +0900

    ss

--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/index.js
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/index.js
@@ -1,7 +1,7 @@
 //인덱스 index.scss 불러오기
 import "../src/css/index.scss";
 
-import React, { useEffect,useRef  } from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Layout from "./components/layout/Layout";
@@ -17,15 +17,20 @@ import Sorry from "./components/pages/Sorry";
 
 import $ from "jquery";
 
-
 export default function MainComponent() {
+  let vh = 0;
+
+  useEffect(() => {
+    vh = window.innerHeight;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, []);
 
   const reName = ["man", "woman", "kids", "home"];
   return (
     //라우터 루트로 브라우저 라우트 시작
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       {/* 라우터 경로 변경시 최상단이동 컴포넌트 */}
-      <WindowScrollTo00/>
+      <WindowScrollTo00 />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Main />} />
@@ -36,12 +41,15 @@ export default function MainComponent() {
           <Route path="aboutzara" element={<AboutZara />} />
           <Route path="search" element={<SearchPg />} />
           <Route path="sorry" element={<Sorry />} />
-          
+
           {reName.map((e, i) => (
-            <Route key={i} path={e + "/product"} element={<ProductPg cat={e} />} />
+            <Route
+              key={i}
+              path={e + "/product"}
+              element={<ProductPg cat={e} />}
+            />
           ))}
           {/* <Route path="product" element={<ProductPg />} /> */}
-          
         </Route>
       </Routes>
     </BrowserRouter>
@@ -52,24 +60,22 @@ export default function MainComponent() {
   컴포넌트로 만들고 라우터 않에 넣고
   라우터 경로 변경시 스크롤 최상단이동
 ***********************************************************************************/
-const WindowScrollTo00=()=>{
+const WindowScrollTo00 = () => {
   // 라우터 경로 변경시 path값 변경하기
   // pathname객체 속성에 담긴다!
-  const {pathname} = useLocation();
-  
-  useEffect(()=>{// 화면렌더링 구역에 스크롤상단이동 코드넣기
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // 화면렌더링 구역에 스크롤상단이동 코드넣기
     // window.scrollTo(0,0); //스크롤 최상단이동!
-    $("html,body").animate({scrollTop: "0px"});
-    console.log("라우터 경로:",pathname);
-  },[pathname]);// 의존성을 라우터 경로변수로 설정한다.(의존성: 쟤가 바뀌면 나도 바뀜)
-  
+    $("html,body").animate({ scrollTop: "0px" });
+    console.log("라우터 경로:", pathname);
+  }, [pathname]); // 의존성을 라우터 경로변수로 설정한다.(의존성: 쟤가 바뀌면 나도 바뀜)
+
   // 컴포넌트 리턴이 필요하나
   // 소스리턴이 아니므로 null을 쓴다.
-  return null;//빈값 리턴(리턴만 되게)
+  return null; //빈값 리턴(리턴만 되게)
 };
-
-
-
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(<MainComponent></MainComponent>);
