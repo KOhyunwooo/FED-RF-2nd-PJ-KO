1545ba779b8bb467b88d6e7b9444a164f475167e
KO <j316ko@naver.com>
2024-07-02 15:43:45 +0900

    124

--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/index.js
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/index.js
@@ -23,16 +23,15 @@ export default function MainComponent() {
 
 
   //88888888888888888888888888888888888888888888888888888888
+
   const setVh = () => {
+    const vh = window.innerHeight * 0.01; 
     // --vh CSS 변수에 현재 뷰포트 높이를 설정하는 함수
-    document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   };
   
-  // 브라우저 창 크기가 변경될 때마다 setVh 함수 호출
+  window.addEventListener('load', setVh);
   window.addEventListener('resize', setVh);
-  
-  // 페이지가 로드될 때 setVh 함수 호출하여 초기화
-  setVh();
 
 //8888888888888888888888888888888888888888888888888888888
 
