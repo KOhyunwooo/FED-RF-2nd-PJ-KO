2f1aa61ef781ff68b31a60368d066d034ec0f1c8
j316 <j316ko@naver.com>
2024-07-10 22:11:18 +0900

    124

--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/pages/DetailPg.jsx
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/pages/DetailPg.jsx
@@ -20,44 +20,51 @@ function DetailPg(props) {
   const [toggle, setToggle] = useState(false);
   //사이즈버튼 클릭시 색상 변경을 위한 상태관리변수
   const [chgcolor, setChgcolor] = useState(null);
-  
 
   return (
-    // 왼쪽 두개+오른쪽 디테일 부분///flex하였음////////////////////////////////////
+    // 전체 박스//flex하였음////////////////////////////////////
     <div className="detailbox">
-      {/* 왼쪽 두개 부분///flex하였음//////////////////////////////////// */}
-      <div className="care_imgbx">
-        <div className="care-box-wrap">
+      {/* 왼쪽 케어부분//////////////////////////////////// */}
+      {/* <div className="care_imgbx"> */}
+      <div className="care-box-wrap">
         <div className={`care-box${toggle ? " on" : ""}`}>
-          
-           <CareTxt/>
-          
+          <CareTxt />
+
           <button
             className="morebt"
-            onClick={() => setToggle((prevState) => !prevState)}
+            onClick={() =>
+              //prevState => !prevState는 상태를 이전상태로 변경 시켜줌,toggle만들때 사용
+              setToggle((prevState) => !prevState)
+            }
+            style={{
+              position: toggle ? "relative" : "absolute",
+              bottom: toggle ? "0px" : "0px", // toggle 상태에 따라 top 위치 설정
+            }}
           >
-            {/* prevState => !prevState는 상태를 이전상태로 변경 시켜줌,toggle만들때 사용 */}
             {toggle == true ? "접기" : "더보기"}
           </button>
         </div>
-        </div>
-        {/* 스와이퍼 디테일 부분 ,detail-img */}
-        <div className="detail-img">
-          <SwiperDetail data={data}/>
-        </div>
       </div>
+      {/*중앙 스와이퍼 부분 ,detail-img */}
+      <div className="detail-img">
+        <SwiperDetail data={data} />
+      </div>
+      {/* </div> */}
 
-      {/* 가장 오른쪽 디테일 부분///flex하였음//////////////////////////////////// */}
+      {/* 오른쪽 디테일 부분///flex하였음//////////////////////////////////// */}
       <div className="detail-txtbox">
         <div className="dttxt-bx">
-         
           <div className="tit">{data.name}</div>
-          <div className="price">{data.price}</div>
+          <div className="price">
+            {data.price[0] && <span>{data.price[0]}</span>}
+            {data.price[1] && (
+              <span>
+                {data.price[1]}&nbsp;{data.price[2]}
+              </span>
+            )}
+          </div>
           <div className="desc">{data.txt}</div>
-          
-          
 
-        
           <ul className="info">
             <li>오프라인 매장에 재고 상태 보기</li>
             <li>배송, 교환 및 반품</li>
@@ -66,30 +73,32 @@ function DetailPg(props) {
         <div className="dtsize-bx">
           <div className="color">{data.color}</div>
           <div className="size">
-            {data.size.map((v,i)=>
-            <button key={i}
-            
-            onClick={(e)=>{
-              setChgcolor(i);
-              
-              //e.자신의.부모요소의.부모요소의.nextElementSibling( 선택요소의 다음요소).style.backgroundColor주기
-              e.currentTarget.parentElement.parentElement.nextElementSibling.style.backgroundColor = "black";
-              
-              //데이터 전역.useRef에 저장하기
-              let dt3 = data.isrc;
-              let dt1 = e.target.innerText;
-              let dt2 = document.querySelector(".color").innerText;
-              myCon.optVal.current = [dt1,dt2,dt3];//전역.useRef.자신 으로 dt1,dt2,dt3을 담기
-            }}
-            style={{
-              backgroundColor: chgcolor === i ? 'black' : 'transparent',
-              color: chgcolor === i ? 'white' : 'black',
-              transition: '0.3s ease, 0.3s ease'
-            }}
-            >{v}</button>
-          
-          )}
-            
+            {data.size.map((v, i) => (
+              <button
+                key={i}
+                className={`${chgcolor === i ? "on" : ""}`}
+                onClick={(e) => {
+                  //순번:i 담아서 적용되게 하기
+                  setChgcolor(i);
+
+                  //e.자신의.부모요소의.부모요소의.nextElementSibling( 선택요소의 다음요소).style주기: 여기에 주는 이유는 size버튼 클릭시 실행되게 하기 위해
+                  e.currentTarget.parentElement.parentElement.nextElementSibling.style.backgroundColor =
+                    "black";
+                  e.currentTarget.parentElement.parentElement.nextElementSibling.style.color =
+                    "#fdfdfd";
+                  e.currentTarget.parentElement.parentElement.nextElementSibling.style.transition =
+                    "0.3s 0.2s ease-in";
+
+                  //데이터 전역.useRef에 저장하기/////////////////
+                  let dt3 = data.isrc;
+                  let dt1 = e.target.innerText;
+                  let dt2 = document.querySelector(".color").innerText;
+                  myCon.optVal.current = [dt1, dt2, dt3]; //전역.useRef.자신 으로 dt1,dt2,dt3을 담기
+                }}
+              >
+                {v}
+              </button>
+            ))}
           </div>
         </div>
         <button
