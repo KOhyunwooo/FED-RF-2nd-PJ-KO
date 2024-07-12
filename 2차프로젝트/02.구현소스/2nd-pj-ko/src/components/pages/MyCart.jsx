cc998ec161c275ff6e028f449eceaa6cc2598165
j316 <j316ko@naver.com>
2024-07-13 01:18:59 +0900

    124

--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/pages/MyCart.jsx
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/pages/MyCart.jsx
@@ -1,72 +1,77 @@
 import React from "react";
 //마이카트 (장바구니)scss불러오기
 import "../../css/my_cart.scss";
-//임시데이터 불러오기
-import { mOrigins } from "../data/products_man";
+
 import { Link } from "react-router-dom";
-//폰트어썸 불러오기
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMinus, faPlus, faXmark } from "@fortawesome/free-solid-svg-icons"; 
-
-
 
 function MyCart(props) {
-    //로컬스 데이터 가져오기///////////////////////////////////////////////////////////
-    const localsData = JSON.parse(localStorage.getItem("mycart-data")) || [];
-    console.log("이걸 장바구니에 뿌려야지", localsData);
+  //로컬스 데이터 가져오기///////////////////////////////////////////////////////////
+  const localsData = JSON.parse(localStorage.getItem("mycart-data")) || [];
+  console.log("이걸 장바구니에 뿌려야지", localsData);
 
-    const xmark=<FontAwesomeIcon icon="fa-light fa-xmark" />
-    return (
-        <>
-            {" "}
-            <div style={{ height: "50px" }}></div>
-            <h1>나는 mycart 장바구니 페이지임</h1>
-            <button className="mycart-button">
-                바스켓백({localsData.length})
-            </button>
-            <button className="favorite-button">마음에 드는 제품</button>
-            <div className="mycart-txt">
-                장바구니에 담긴 상품은 구매가 완료될 때까지 예약되지 않습니다.
-            </div>
-            <div className="mycart-box">
-                <div className="product-list5">
-                    {localsData.map((v, i) => (
-                        <div key={i} className="product-item chgop">
-                            <Link to="/">
-                                <img
-                                    src={process.env.PUBLIC_URL + v.isrc}
-                                    alt={v.name}
-                                    className="product-image"
-                                />
-                            </Link>
-                            <div className="txt-box">
-                            {/* <FontAwesomeIcon icon="fa-light fa-xmark" /> */}
-                            <FontAwesomeIcon icon={faXmark} size="3x"  />
-                            <FontAwesomeIcon icon="fa-light fa-xmark" />
-                            <FontAwesomeIcon icon={faPlus} size="3x"/>
-                            <FontAwesomeIcon icon={faMinus} size="3x" />
-                            {xmark}
-                                <span>{v.name}</span>
-                                <span className="price">
-                                {v.price && <p>{v.price}</p>}
-                                    {v.price1 && (
-                                        <p>
-                                            {v.price1}&nbsp;{v.price2}
-                                        </p>
-                                    )}
-                                    
-                                </span>
-                                <span>
-                                    {v.size} | {v.color}
-                                </span>
-                            </div>
-                        </div>
-                    ))}
+  return (
+    <>
+      {" "}
+      <div style={{ height: "50px" }}></div>
+      <h1>나는 mycart 장바구니 페이지임</h1>
+      <button className="mycart-button">바스켓백({localsData.length})</button>
+      <button className="favorite-button">마음에 드는 제품</button>
+      <div className="mycart-txt">
+        장바구니에 담긴 상품은 구매가 완료될 때까지 예약되지 않습니다.
+      </div>
+      <div className="mycart-box">
+        <div className="product-list5">
+          {localsData.map((v, i) => (
+            <div key={i} className="product-item chgop">
+              <Link to="/">
+                <img
+                  src={process.env.PUBLIC_URL + v.isrc}
+                  alt={v.name}
+                  className="product-image"
+                />
+              </Link>
+              <div className="txt-box">
+                
+                {/* <img src={process.env.PUBLIC_URL+"/images/icons/multiply.png"} alt="minus" /> */}
+
+                <span>{v.name}</span>
+                <span className="price">
+                  {v.price && <p>{v.price}</p>}
+                  {v.price1 && (
+                    <p>
+                      {v.price1}&nbsp;{v.price2}
+                    </p>
+                  )}
+                </span>
+                <span>
+                  {v.size} | {v.color}
+                </span>
+
+
+                  {/* +-버튼 박스 */}
+                <div className="cntbox">
+                  <button>
+                    <img
+                      src={process.env.PUBLIC_URL + "/images/icons/plus.png"}
+                      alt="plus"
+                    />
+                  </button>
+                  <input type="text" readOnly value={1} />
+                  <button>
+                    <img
+                      src={process.env.PUBLIC_URL + "/images/icons/minus.png"}
+                      alt="minus"
+                    />
+                  </button>
                 </div>
+              </div>
             </div>
-            <h1>여기에 추천상품 들어올것↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓</h1>
-        </>
-    );
+          ))}
+        </div>
+      </div>
+      <h1>여기에 추천상품 들어올것↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓</h1>
+    </>
+  );
 }
 
 export default MyCart;
