85ff9636ef3b7db3f7e7bdeb783df4c3ba74f45d
j316 <j316ko@naver.com>
2024-07-15 00:39:00 +0900

    12412

--- 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/modules/Favorite.jsx
+++ 2차프로젝트/02.구현소스/2nd-pj-ko/src/components/modules/Favorite.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Favorite(props) {
     return (
         <div>
-            <h1>나는 즐겨찾기 페이지~</h1>
-            <h1>나는 즐겨찾기 페이지~</h1>
-            <h1>나는 즐겨찾기 페이지~</h1>
+            <h1>나는 즐겨찾기 모듈~</h1>
+            <h1>나는 즐겨찾기 모듈~</h1>
+            <h1>나는 즐겨찾기 모듈~</h1>
         </div>
     );
 }
